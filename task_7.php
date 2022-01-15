5dab55a20cc00250f2b8cf3083e5efa913e595dd
Богуславський Кирило Олексійович <k.boguslavskyi@temabit.com>
2022-01-15 16:30:26 +0200

    15/01/2021

---
+++ task_7.php
@@ -0,0 +1,82 @@
+<?php
+$pdo = new PDO("mysql:host=localhost; dbname=rahimbd", "root", "");
+
+$sql = "select * from lessons";
+$result = $pdo->query($sql);
+
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>
+        Подготовительные задания к курсу
+    </title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+    <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+</head>
+<body class="mod-bg-1 mod-nav-link ">
+<main id="js-page-content" role="main" class="page-content">
+    <div class="col-md-6">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    Задание
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+ 
+                        <?php
+                       
+                        foreach ($result as $a) {
+                            echo '<div class="';
+                            echo ($a[ban] != 1) ? '' : 'banned ';
+                            echo 'rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">';
+                            echo '<img src="'.$a[img].'" alt="'.$a[alt].'" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">';
+                            echo '<div class="ml-2 mr-3">';
+                            echo '<h5 class="m-0">';
+                            echo $a[title];
+                            echo '<small class="m-0 fw-300">';
+                            echo $a[job];
+                            echo '</small></h5>';
+                            echo '<a href="'.$a[link].'" class="text-info fs-sm" target="_blank">'.$a[mail].'</a> - ';
+                            echo '<a href="'.$a[link2].'" class="text-info fs-sm" target="_blank" title="'.$a[title2].'"><i class="fal fa-envelope"></i></a>';
+                            echo '</div></div>';
+                        }
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script src="js/vendors.bundle.js"></script>
+<script src="js/app.bundle.js"></script>
+<script>
+    // default list filter
+    initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+    // custom response message
+    initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+</script>
+</body>
+</html>
+
+                        
